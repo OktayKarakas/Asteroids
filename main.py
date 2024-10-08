d2d9d7d384a87a86498a9720237e03b3c1714037
Oktay Karakaş <oktaykarakas71@gmail.com>
2024-10-08 15:48:49 +0300

    fps lock

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ def main():
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        delta = time_passed / 1000
 if __name__ == "__main__":
     main()
