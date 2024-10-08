107d3b45df485ae3c6f71c694dca2479712a661a
Oktay Karakaş <oktaykarakas71@gmail.com>
2024-10-08 18:10:05 +0300

    player can move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -7,14 +8,18 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         time_passed = clock.tick(60)
-        delta = time_passed / 1000
+        dt = time_passed / 1000
+        
 if __name__ == "__main__":
     main()
